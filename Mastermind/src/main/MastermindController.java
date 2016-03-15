de11f2e6b0b5f86f62b5c537d530dff5945f0b0d
Jordan <jwells.me@gmail.com>
2016-03-15 08:06:36 -0400

    Controller added

---
+++ Mastermind/src/main/MastermindController.java
@@ -0,0 +1,215 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MastermindController implements Initializable {
+
+    @FXML
+    private Circle computerMove3Image;
+
+    @FXML
+    private Circle computerMove2Slot;
+
+    @FXML
+    private Circle passwordMove1Slot;
+
+    @FXML
+    private Circle oldMove1Slot;
+
+    @FXML
+    private Button passwordSaveButton;
+
+    @FXML
+    private HBox oldMoveBox;
+
+    @FXML
+    private Circle passwordMove2Slot;
+
+    @FXML
+    private Circle passwordMove3Image1;
+
+    @FXML
+    private Rectangle oldMoveBackground;
+
+    @FXML
+    private Circle computerMove2Image;
+
+    @FXML
+    private Circle passwordMove2Image;
+
+    @FXML
+    private ComboBox<?> passwordColorSelector;
+
+    @FXML
+    private Circle oldMove4Slot;
+
+    @FXML
+    private Circle computerMove3Slot;
+
+    @FXML
+    private Circle passwordMove3Slot;
+
+    @FXML
+    private AnchorPane anchorpane;
+
+    @FXML
+    private Circle oldMove3Slot;
+
+    @FXML
+    private Rectangle oldMovePeg4;
+
+    @FXML
+    private Rectangle oldMovePeg3;
+
+    @FXML
+    private Rectangle oldMovePeg2;
+
+    @FXML
+    private Rectangle oldMovePeg1;
+
+    @FXML
+    private Circle passwordMove1Image;
+
+    @FXML
+    private Circle computerMove4Slot;
+
+    @FXML
+    private Button passwordRandomButton;
+
+    @FXML
+    private Circle oldMove2Slot;
+
+    @FXML
+    private Circle passwordMove4Slot;
+
+    @FXML
+    private HBox computerMoves;
+
+    @FXML
+    private Circle passwordMove3Image;
+
+    @FXML
+    private Circle computerMove1Image;
+
+    @FXML
+    private Rectangle computerMovePeg1;
+
+    @FXML
+    private GridPane computerMovePegs;
+
+    @FXML
+    private Rectangle computerMovePeg3;
+
+    @FXML
+    private Circle computerMove1Slot;
+
+    @FXML
+    private Rectangle computerMovePeg2;
+
+    @FXML
+    private Circle computerMove4Image;
+
+    @FXML
+    private Rectangle computerMovePeg4;
+
+    @FXML
+    void setSlotColor(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+//    @FXML
+//    void 1f1c32(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void b21212(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 00000082(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 616161f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 636363f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 636363f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 636363f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void ff7777(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void ff7777(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void ff7777(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void ff7777(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 616161f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 616161f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 616161f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 616161f5(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void 0000009f(ActionEvent event) {
+//
+//    }
+
+}
